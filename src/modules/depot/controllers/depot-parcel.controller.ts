54fbf0ab927a779c064c3dcfb07f2ee1a57aaf44
Kainar Masujima <masujimk@gmail.com>
2023-01-25 13:03:07 +0600

    fix: fix swagger response type

--- src/modules/depot/controllers/depot-parcel.controller.ts
+++ src/modules/depot/controllers/depot-parcel.controller.ts
@@ -1,5 +1,4 @@
 import { Body, Controller, Param, Post, UseGuards } from '@nestjs/common';
-import { ApiResponse } from '@nestjs/swagger';
 
 import { HasDepotStaffRoles } from '@/common/decorators/depot-staff-roles.decorator';
 import { ReqUser } from '@/common/decorators/req-user.decorator';
@@ -7,7 +6,6 @@ import type { UserPayload } from '@/modules/auth/auth.interface';
 import { IsDepotStaffGuard } from '@/modules/auth/guards/is-depot-staff.guard';
 import { JwtAuthGuard } from '@/modules/auth/guards/jwt-auth.guard';
 
-import { DepotResponseDto } from '../dto/depot.response.dto';
 import { CreateParcelBodyDto } from '../dto/parcel-management/create-parcel.body.dto';
 import { DepotStaffRoleType } from '../enum/depot-staff.enum';
 import { ParcelManagementService } from '../services/parcel-management.service';
@@ -18,7 +16,6 @@ export class DepotParcelController {
 
   @HasDepotStaffRoles(DepotStaffRoleType.MANAGER, DepotStaffRoleType.ADMIN)
   @UseGuards(JwtAuthGuard, IsDepotStaffGuard)
-  @ApiResponse({ type: [DepotResponseDto] })
   @Post('')
   public async createParcel(
     @Param('depot') depotId: number,
